8f3c82368de0df5ae92ff1f90d10264fa2a246a0
Quasmati <125371562+quasmati@users.noreply.github.com>
2023-08-04 20:35:58 -0500

    app.py

--- app.py
+++ app.py
@@ -8,12 +8,16 @@ load_dotenv()
 openai.api_key = os.getenv('API_KEY')
 
 
-def chat(message):
+
+
+def chat(message, history=None):
+    if history is None:
+        history = []
     try:
         response = openai.Completion.create(
             engine="text-davinci-003",
             prompt=message,
-            max_tokens=4089,
+            max_tokens=4086, # Adjusted max_tokens value
             n=1,
             stop=None,
             temperature=0.7,
@@ -21,13 +25,25 @@ def chat(message):
         message = response.choices[0].text.strip()
     except Exception as e:
         message = f"Error: {str(e)}"
+    history.append((message, "ChatGPT"))
+    return message
+
+chat_history = []
+
+def chatbot(prompt):
+    global chat_history
+    message = chat(prompt, chat_history)
+    if message is not None:
+        chat_history.append((prompt, message))
+    else:
+        message = "Error: No response."
     return message
 
 inputs = gr.inputs.Textbox(label="Enter a prompt")
 outputs = gr.outputs.Textbox(label="Chatbot response")
 
-title = "🏳️‍⚧️ quasGPT-Assistant 💕"
+title = "🤖 ChatGPT-Assistant 🐍"
 description = "<center> ChatGPT-Assistant is a chatbot that uses the OpenAI GPT-3 model. <center>"
 
-interface = gr.Interface(fn=chat, inputs=inputs, outputs=outputs, title=title, description=description)
-interface.launch()
+interface = gr.Interface(fn=chatbot, inputs=inputs, outputs=outputs, title=title, description=description)
+interface.launch(share=True)
